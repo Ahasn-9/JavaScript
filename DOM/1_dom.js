16c0191da1fd3a998b11d7b65fbeed737f6f4a13
Ahasn-9 <sp21-bse-024@cuilahore.edu.pk>
2025-02-28 00:10:10 +0500

    add , remove , edit

--- DOM/1_dom.js
+++ DOM/1_dom.js
@@ -1,6 +1,21 @@
 // selecting the element by class name and converting it into an array and changing the color using forEach loop
 
-const list = document.getElementsByClassName("classList");
-Array.from(list).forEach((element) => {
-  element.style.color = "red";
-});
+function addLanguage(langName) {
+  const li = document.createElement("li");
+  li.innerHTML = `${langName}`;
+  document.querySelector(".language").appendChild(li);
+}
+
+addLanguage("JavaScript");
+addLanguage("Java");
+addLanguage("C++");
+
+//edit
+const secondLang = document.querySelector("li:nth-child(2)");
+const newLi = document.createElement("li");
+newLi.innerHTML = "lhgkshgo";
+secondLang.replaceWith(newLi);
+
+//delete
+const thirdLang = document.querySelector("li:nth-child(4)");
+thirdLang.remove();
